8890803e48ad601b236971d9d5dcacfc676d4cfb
Levani Bukhnikashvili <levani_bukhnikashvili@epam.com>
2025-02-14 18:35:34 +0400

    implement GameController

---
+++ Gamestore/Controllers/GamesController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gamestore.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class GamesController(IGameService gameService) : ControllerBase
+{
+    private readonly IGameService _gameService = gameService;
+
+    [HttpGet]
+    public async Task<IEnumerable<GameModel>> Get()
+    {
+        return await _gameService.GetAllAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<GameModel?> Get(int id)
+    {
+        return await _gameService.GetByIdAsync(id);
+    }
+
+    [HttpPost]
+    public async Task Post([FromBody] GameModel game)
+    {
+        await _gameService.AddAsync(game);
+    }
+
+    [HttpPut]
+    public async Task Put([FromBody] GameModel game)
+    {
+        await _gameService.UpdateAsync(game);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task Delete(Guid id)
+    {
+        await _gameService.DeleteAsync(id);
+    }
+}
---
+++ Gamestore/Startup.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Business;
+using Business.Interfaces;
+using Business.Services;
+using Data.Data;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gamestore;
+
+public class Startup(IConfiguration configuration)
+{
+    public IConfiguration Configuration { get; } = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<GamestoreDBContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        var mapperConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new AutomapperProfile());
+        });
+        IMapper mapper = mapperConfig.CreateMapper();
+        services.AddSingleton(mapper);
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IGameService, GameService>();
+        services.AddScoped<IPlatformService, PlatformService>();
+        services.AddScoped<IGenreService, GenreService>();
+
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
