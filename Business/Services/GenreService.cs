5ec669d26ec73a14bf4f6427fd7fe4c7df68c1f6
Levani Bukhnikashvili <levani_bukhnikashvili@epam.com>
2025-02-24 15:30:07 +0400

    Remove the member initializer

--- Business/Services/GenreService.cs
+++ Business/Services/GenreService.cs
@@ -9,14 +9,10 @@ namespace Business.Services
 {
 	public class GenreService(IUnitOfWork unitOfWork, IMapper mapper) : IGenreService
 	{
-		private readonly IUnitOfWork unitOfWork = unitOfWork;
-
-		private IMapper Mapper { get; } = mapper;
-
 		public async Task AddAsync(GenreModel model)
 		{
 			await ValidateGenre(model);
-			var genre = Mapper.Map<Genre>(model);
+			var genre = mapper.Map<Genre>(model);
 			await unitOfWork.GenreRepository!.AddAsync(genre);
 			await unitOfWork.SaveAsync();
 		}
@@ -30,31 +26,31 @@ namespace Business.Services
 		public async Task<IEnumerable<GenreModel>> GetAllAsync()
 		{
 			var genres = await unitOfWork.GenreRepository!.GetAllAsync();
-			return Mapper.Map<IEnumerable<GenreModel>>(genres);
+			return mapper.Map<IEnumerable<GenreModel>>(genres);
 		}
 
 		public async Task<IEnumerable<GenreModel?>> GetGenresByParentId(Guid id)
 		{
 			var genres = await unitOfWork.GenreRepository!.GetAllAsync(g => g.ParentGenreId == id);
-			return Mapper.Map<IEnumerable<GenreModel?>>(genres);
+			return mapper.Map<IEnumerable<GenreModel?>>(genres);
 		}
 
 		public async Task<GenreModel?> GetByIdAsync(object id)
 		{
 			var genre = await unitOfWork.GenreRepository!.GetByIDAsync(id);
-			return Mapper.Map<GenreModel?>(genre);
+			return mapper.Map<GenreModel?>(genre);
 		}
 
 		public async Task<IEnumerable<GameModel?>> GetGamesByGenreIdAsync(Guid id)
 		{
 			var games = await unitOfWork.GameRepository!.GetAllAsync(g => g.Genres.Select(g => g.Id).Contains(id));
-			return Mapper.Map<IEnumerable<GameModel?>>(games);
+			return mapper.Map<IEnumerable<GameModel?>>(games);
 		}
 
 		public async Task UpdateAsync(GenreModel model)
 		{
 			await ValidateGenre(model);
-			var genre = Mapper.Map<Genre>(model);
+			var genre = mapper.Map<Genre>(model);
 			unitOfWork.GenreRepository!.Update(genre);
 			await unitOfWork.SaveAsync();
 		}
